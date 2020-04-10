e841fcb91ee2b7b7d198e19990c0c3d0dd21f94c
Yuki Mihashi <31533303+yuki384@users.noreply.github.com>
2020-04-10 19:28:14 +0900

    layout style of content

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,51 @@
 <template>
   <section>
     <div>
-      <div class="content-one">
-        <div class="content-thumbnail">
-          <img src="~/assets/emoji/1f3e0.svg" alt="" />
+      <div class="content">
+        <div class="content-one">
+          <div class="content-thumbnail">
+            <img src="~/assets/emoji/1f3e0.svg" alt="" />
+          </div>
+          <h3 class="content-title">うちで過ごそう！</h3>
+          <shareButtons
+            url="stayhome"
+            text="うちで過ごそう！みんなのために🏠
+            #うちで過ごそう #stayhome #Yobikake"
+          ></shareButtons>
         </div>
-        <h3 class="content-title">うちで過ごそう！</h3>
-        <shareButtons
-          url="stayhome"
-          text="うちで過ごそう！みんなのために🏠
-          #うちで過ごそう #stayhome #Yobikake"
-        ></shareButtons>
-      </div>
-      <div class="content-one">
-        <div class="content-thumbnail">
-          <img src="~/assets/emoji/1f932.svg" alt="" />
+        <div class="content-one">
+          <div class="content-thumbnail">
+            <img src="~/assets/emoji/1f932.svg" alt="" />
+          </div>
+          <h3 class="content-title">こまめな手洗いを</h3>
+          <shareButtons
+            url="handhygiene"
+            text="こまめな手洗いをしよう！🧼🤲✨
+            #こまめな手洗いを #handhygiene #Yobikake"
+          ></shareButtons>
         </div>
-        <h3 class="content-title">こまめな手洗いを</h3>
-        <shareButtons
-          url="handhygiene"
-          text="こまめな手洗いをしよう！🧼🤲✨
-          #こまめな手洗いを #handhygiene #Yobikake"
-        ></shareButtons>
-      </div>
-      <div class="content-one">
-        <div class="content-thumbnail">
-          <img src="~/assets/emoji/1f44f.svg" alt="" />
+        <div class="content-one">
+          <div class="content-thumbnail">
+            <img src="~/assets/emoji/1f44f.svg" alt="" />
+          </div>
+          <h3 class="content-title">医療に携わる人へ感謝を</h3>
+          <shareButtons
+            url="thankyou"
+            text="医療に携わる人へ感謝を伝えよう！👨‍⚕️👩‍🔬👏
+            #感謝を伝えよう #thankyou #Yobikake"
+          ></shareButtons>
         </div>
-        <h3 class="content-title">医療に携わる人へ感謝を</h3>
-        <shareButtons
-          url="thankyou"
-          text="医療に携わる人へ感謝を伝えよう！👨‍⚕️👩‍🔬👏
-          #感謝を伝えよう #thankyou #Yobikake"
-        ></shareButtons>
-      </div>
-      <div class="content-one">
-        <div class="content-thumbnail">
-          <img src="~/assets/emoji/1f637.svg" alt="" />
+        <div class="content-one">
+          <div class="content-thumbnail">
+            <img src="~/assets/emoji/1f637.svg" alt="" />
+          </div>
+          <h3 class="content-title">距離をたもとう</h3>
+          <shareButtons
+            url="keepdistance"
+            text="💚🧍‍♂️人との距離をたもとう🧍‍♀️💜
+            #距離をたもとう #keepdistance #Yobikake"
+          ></shareButtons>
         </div>
-        <h3 class="content-title">距離をたもとう</h3>
-        <shareButtons
-          url="keepdistance"
-          text="💚🧍‍♂️人との距離をたもとう🧍‍♀️💜
-          #距離をたもとう #keepdistance #Yobikake"
-        ></shareButtons>
       </div>
     </div>
   </section>
@@ -66,10 +68,17 @@ body {
     box-sizing: border-box;
   }
 }
+.content {
+  width: 80%;
+  display: flex;
+  flex: flex-wrap;
+  margin: auto;
+}
 .content-one {
   background-color: #fff;
-  width: 20%;
+  width: 25%;
   border-radius: 8px;
+  margin: 12px;
 }
 .content-thumbnail {
   background-color: #fea63e;
