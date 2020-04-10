727d9bf2432140ab513177e018c61ad9dc95b9c4
Tomohiro <tomo.nz3156@gmail.com>
2020-04-10 18:51:51 +0900

    Add header

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="logo">
+    <img src="~/assets/logo.svg" alt="Logo" />
+    <h3>あなたの呼びかけからはじめよう</h3>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  text-align: center;
+
+  img {
+    width: 30%;
+  }
+}
+</style>
