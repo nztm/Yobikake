108a4f64188057570647064752ea32fe4fb22467
Tomohiro <tomo.nz3156@gmail.com>
2020-04-10 19:55:43 +0900

    Reset layout.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,7 @@
 <template>
   <div>
-    <Header />
     <nuxt />
-    <Footer />
   </div>
 </template>
-
-<script>
-import Header from '~/components/Header.vue'
-import Footer from '~/components/Footer.vue'
-
-export default {
-  components: {
-    Header,
-    Footer
-  }
-}
-</script>
 
 <style></style>
