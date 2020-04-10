6a8f6b1d881c1aed0d45fdb4f6c7f7f2b33eb7fe
Yuki Mihashi <31533303+yuki384@users.noreply.github.com>
2020-04-11 07:24:43 +0900

    style for about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <section class="text-content">
+    </section>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style>
+.text-content {
+  width: 90%;
+  max-width: 900px;
+  margin: 24px auto;
+  line-height: 2em;
+}
+</style>
