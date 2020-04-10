d5b4b18e9d9e3028bd9c901d13aee8e5be08df84
Yuki Mihashi <31533303+yuki384@users.noreply.github.com>
2020-04-10 13:45:32 +0900

    Style share buttons

--- pages/index.vue
+++ pages/index.vue
@@ -9,19 +9,19 @@
         <a
           href="http://twitter.com/share?url=https://yobikake.com/stayhome&hashtags=Yobikake,うちで過ごそう"
           target="_blank"
-          class="share-link"
+          class="share-link share-link-twitter"
           ><i class="fab fa-twitter"></i> Twitter でシェア</a
         >
         <a
           href="https://www.facebook.com/dialog/share?app_id=????&display=popup&href=https://yobikake.com/stayhome&hashtag=#Yobikake"
           target="_blank"
-          class="share-link"
+          class="share-link share-link-fb"
           ><i class="fab fa-facebook"></i> Facebook でシェア</a
         >
         <a
           href="https://social-plugins.line.me/lineit/share?url=https://yobikake.com/stayhome"
           target="_blank"
-          class="share-link"
+          class="share-link share-link-line"
           ><i class="fab fa-line"></i> LINE でシェア</a
         >
       </div>
@@ -29,4 +29,23 @@
   </section>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.share-link {
+  display: block;
+  border-radius: 4px;
+  padding: 12px 24px;
+  margin: 8px;
+
+  color: #fff;
+  text-decoration: none;
+}
+.share-link-twitter {
+  background-color: #1da1f2;
+}
+.share-link-fb {
+  background-color: #3b5998;
+}
+.share-link-line {
+  background-color: #00bb00;
+}
+</style>
