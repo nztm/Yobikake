ede50b3f0d221f6021ef59a4c4c19952c757ed16
Tomohiro <tomo.nz3156@gmail.com>
2020-04-10 18:31:23 +0900

    Add footer

---
+++ components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <footer>
+      <nav class="footer-menu">
+        <ul class="footer-menu-list">
+          <li><nuxt-link to="/about">このサイトについて</nuxt-link></li>
+          <li>
+            <a
+              href="https://github.com/nztm/yobikake.com/blob/master/CONTRIBUTORS.md"
+              target="_blank"
+              rel="noopener"
+              >Contributors</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://github.com/nztm/yobikake.com"
+              target="_blank"
+              rel="noopener"
+              >GitHub</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/yobikakecom"
+              target="_blank"
+              rel="noopener"
+              >Twitter</a
+            >
+          </li>
+        </ul>
+      </nav>
+      <p class="copyright">
+        &copy; 2020 Yobikake |
+        <a href="https://twemoji.twitter.com/" target="_blank" rel="noopener"
+          >Twemoji CC-BY 4.0</a
+        >
+      </p>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+// footer {
+//   padding: 2.4em;
+//   background-color: #7bb8ff;
+//   color: #fff;
+// }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <nuxt />
+    <Footer />
   </div>
 </template>
+
+<script>
+import Footer from '~/components/Footer.vue'
+
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
 
 <style></style>
