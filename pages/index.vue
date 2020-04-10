63337a72c78f0c45230fb3760183915856a24284
Tomohiro <tomo.nz3156@gmail.com>
2020-04-10 23:09:00 +0900

    Add shadow

--- pages/index.vue
+++ pages/index.vue
@@ -77,6 +77,7 @@ export default {
 .content-one {
   background-color: #fff;
   border-radius: 8px;
+  box-shadow: 0 0 15px rgba(100, 100, 100, 0.2);
   margin: 20px;
   width: 280px;
   @media screen and (max-width: 670px) {
