d9dd3a7422bc24de6cf49837d1ddcfd91b515c03
Yuki Mihashi <31533303+yuki384@users.noreply.github.com>
2020-04-10 13:28:50 +0900

    Add share link and default content(ex: img, title)

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,28 @@
 <template>
   <section>
-    <div></div>
+    <div>
+      <div class="content-one">
+        <div class="content-thumbnail">
+          <img src="~/assets/emoji/1f3e0.svg" alt="" />
+        </div>
+        <h3 class="content-title">うちで過ごそう！</h3>
+        <a
+          href="http://twitter.com/share?url=https://yobikake.com/stayhome&hashtags=Yobikake,うちで過ごそう"
+          target="_blank"
+          >Twitter でシェア</a
+        >
+        <a
+          href="https://www.facebook.com/dialog/share?app_id=????&display=popup&href=https://yobikake.com/stayhome&hashtag=#Yobikake"
+          target="_blank"
+          >Facebook でシェア</a
+        >
+        <a
+          href="https://social-plugins.line.me/lineit/share?url=https://yobikake.com/stayhome"
+          target="_blank"
+          >LINE でシェア</a
+        >
+      </div>
+    </div>
   </section>
 </template>
 
