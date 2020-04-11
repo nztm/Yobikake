bba68f588eff92f27a8217c052aa808c70247b22
Tomohiro <tomo.nz3156@gmail.com>
2020-04-11 11:55:29 +0900

    Add contents to about

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
-    <section class="text-content"></section>
+    <section class="text-content">
+      <h1>当サイトについて</h1>
+      <p>
+        「Yobikake」は、SNSを使ってワンクリックで誰もが呼びかけを発信できるサービスです。<br />
+        新型コロナウィルスの感染が拡大するなか、高校生の自分たちにできることはないかと考え、立ち上げました。<br /><br />
+        あなたの呼びかけからはじめよう。皆さんの力を合わせて新型コロナウィルスの拡大を抑えましょう！<br />
+        一人ひとりの呼びかけが大きな力になります。ぜひご自身のSNSでシェアをお願いします。
+      </p>
+      <h2>Google Analyticsの利用について</h2>
+      <p>
+        当サイトでは、Googleによるアクセス解析ツール「Google
+        Analytics」を利用しています。<br />
+        このGoogle
+        Analyticsはトラフィックデータの収集のためにCookieを使用しています。<br />
+        このトラフィックデータは匿名で収集されており、個人を特定するものではありません。<br />
+        この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。<br />
+        GoogleAnalyticsの利用規約及びプライバシーポリシーについては下記のサイトをご覧ください。<br />
+        <a href="https://marketingplatform.google.com/about/analytics/terms/jp/"
+          >Google Analytics 利用規約</a
+        ><br />
+        <a href="https://policies.google.com/technologies/partner-sites?hl=ja"
+          >Google プライバシポリシー</a
+        >
+      </p>
+      <h2>お問い合わせ先</h2>
+      <p>
+        nztm.biz＠gmail.com <br />
+        ※＠を半角にしてください。
+      </p>
+      <a nuxt-link="/">トップに戻る</a>
+    </section>
   </div>
 </template>
 
