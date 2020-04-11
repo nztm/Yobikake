9c411083f9fee2795ea7d51998ac66b8ed42089a
Tomohiro <tomo.nz3156@gmail.com>
2020-04-11 12:57:00 +0900

    Add link to logo

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="logo">
-    <img src="~/assets/logo.svg" alt="Logo" />
+    <nuxt-link to="/">
+      <img src="~/assets/logo.svg" alt="Logo" />
+    </nuxt-link>
     <h3><span>あなたの呼びかけから</span><span>はじめよう</span></h3>
   </div>
 </template>
