1e4c332e704855dfa10b990528f8d7eb11ea70a6
Tomohiro <tomo.nz3156@gmail.com>
2020-04-12 10:44:15 +0900

    Add style scoped setting

--- components/shareButtons.vue
+++ components/shareButtons.vue
@@ -43,7 +43,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .share-buttons {
   padding: 12px;
 }
--- pages/index.vue
+++ pages/index.vue
@@ -61,7 +61,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .content {
   display: flex;
   flex-wrap: wrap;
