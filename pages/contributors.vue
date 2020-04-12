9dbce63fde4cdc1e92eaf42728d52a32ce51f22e
Yuki Mihashi <31533303+yuki384@users.noreply.github.com>
2020-04-12 13:37:07 +0900

    square icon

--- pages/contributors.vue
+++ pages/contributors.vue
@@ -7,10 +7,10 @@
         <h3>Tomohiro Nozaki</h3>
         <div class="sns-link">
           <a href="" target="_blank">
-            <i class="fab fa-github"></i>
+            <i class="fab fa-github-square"></i>
           </a>
           <a href="" target="_blank">
-            <i class="fab fa-twitter"></i>
+            <i class="fab fa-twitter-square"></i>
           </a>
         </div>
       </div>
@@ -19,10 +19,10 @@
         <h3>Yuki Mihashi</h3>
         <div class="sns-link">
           <a href="https://github.com/yuki384" target="_blank">
-            <i class="fab fa-github"></i>
+            <i class="fab fa-github-square"></i>
           </a>
           <a href="https://twitter.com/YukiMihashi" target="_blank">
-            <i class="fab fa-twitter"></i>
+            <i class="fab fa-twitter-square"></i>
           </a>
           <a href="http://yuki384.github.io/" target="_blank">
             <i class="fas fa-globe"></i>
