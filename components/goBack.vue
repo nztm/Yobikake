d9b00893590964b9d15c27e8fa29584b0f638c76
Tomohiro <tomo.nz3156@gmail.com>
2020-04-12 09:27:09 +0900

    Replace goBack to component

---
+++ components/goBack.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <nuxt-link to="/" class="goback">← トップページに戻る</nuxt-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.goback {
+  background-color: #418fb5;
+  border-radius: 40px;
+  display: block;
+  padding: 12px 36px;
+  margin: 60px auto;
+  text-decoration: none;
+  color: #fff;
+  font-weight: bold;
+  width: 90%;
+  max-width: 300px;
+  text-align: center;
+  transition: 0.3s all;
+  box-sizing: border-box;
+  box-shadow: 0 0 15px rgba(8, 28, 115, 0.2);
+  &:hover {
+    padding-left: 0;
+  }
+}
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -33,13 +33,18 @@
         nztm.biz＠gmail.com <br />
         ※＠を半角にしてください。
       </p>
-      <nuxt-link to="/" class="goback">← トップページに戻る</nuxt-link>
+      <goBack />
     </section>
   </div>
 </template>
 
 <script>
+import goBack from '~/components/goBack.vue'
+
 export default {
+  components: {
+    goBack
+  },
   data() {
     return {
       meta: {
@@ -72,25 +77,6 @@ export default {
 
   img {
     width: 20%;
-  }
-}
-.goback {
-  background-color: #418fb5;
-  border-radius: 40px;
-  display: block;
-  padding: 12px 36px;
-  margin: 60px auto;
-  text-decoration: none;
-  color: #fff;
-  font-weight: bold;
-  width: 90%;
-  max-width: 300px;
-  text-align: center;
-  transition: 0.3s all;
-  box-sizing: border-box;
-  box-shadow: 0 0 15px rgba(8, 28, 115, 0.2);
-  &:hover {
-    padding-left: 0;
   }
 }
 </style>
