7e02adbef3b19423af3f9feb9d0a9d61f48b2f60
Tomohiro <tomo.nz3156@gmail.com>
2020-04-15 20:16:16 +0900

    Set lang ja

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
    */
   head: {
     title: 'Yobikake | あなたの呼びかけからはじめよう',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
